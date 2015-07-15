6476a375bb78974412a0dfaefa8ec87207b8bd8b
ramakrishna13 <rdokku@my.harrisburgu.edu>
2015-07-15 15:24:26 -0500

    Assignment2

---
+++ Angular/js/Controller.js
@@ -0,0 +1,72 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', function MyController($scope) {
+    $scope.course = [
+    {
+        "name":"500",
+        "subject":"IS Planning, Engg & Mgmt Principles",
+    },
+    {
+        "name":"501",
+        "subject":"Information & Communication Technologies"
+    },
+    {
+        "name":"502",
+        "subject":"User Centered Design"
+    },
+    {
+        "name":"510",
+        "subject":"Business Strategy & Management Principles"
+    },
+    {
+        "name":"540",
+        "subject":"Architecture and Integration of Modern Enterprises "
+    },
+    {
+        "name":"511",
+        "subject":"Digital and Global Organizations "
+    },
+    {
+        "name":"503",
+        "subject":"Information & Communication Technologies "
+    },
+    {
+        "name":"530",
+        "subject":"Analysis & Design of Information Systems "
+    },
+    {
+        "name":"550",
+        "subject":" Information Security Management"
+    },
+ {
+        "name":"555",
+        "subject":" Mobile Computing and Wireless Communication"
+    },
+    {
+        "name":"565",
+        "subject":"  Business Intelligence and Decision Support "
+    },
+    {
+        "name":"535",
+        "subject":"  Business Process Modeling and Workflow"
+    },
+    {
+        "name":"539",
+        "subject":" Enterprise Architectures and Frameworks"
+    },
+    {
+        "name":"577",
+        "subject":"  Independent Study or Special Topics"
+    },
+    {
+        "name":"GRAD 695",
+        "subject":"  Research Methods and Writing"
+    },
+    {
+        "name":"GRAD 699",
+        "subject":" Applied Project or Research Thesis"
+    },
+       
+]
+});
+
